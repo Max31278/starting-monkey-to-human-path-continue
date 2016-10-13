ca5d0382dfbf35ecd14e8fbcc56e74a49de191d7
Max31278 <max_koval31278@mail.ru>
2016-10-13 12:33:58 +0400

    add TestXmlTask

---
+++ learn/xml/TestXmlTask.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PO41.Koval.wdad.learn.xml;
+
+/**
+ *
+ * @author 000
+ */
+public class TestXmlTask {
+    public static void main(String[] args){
+        try {
+            XmlTask test = new XmlTask();
+            test.setTariffs("electricity", 10.5);
+            System.out.println(test.getBill("Specialistov", 11, 11));
+            test.addRegistration("Specialistov", 20, 1, 2016, 2, 339, 226, 159, 125);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
