96c6055beecd29e148d90976afee62e83e3b50ed
Max31278 <max_koval31278@mail.ru>
2017-01-16 18:41:35 +0400

    rmi

---
+++ learn/rmi/XmlDataManagerImpl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PO41.Koval.wdad.learn.rmi;
+ 
+import PO41.Koval.wdad.learn.xml.XmlTask;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 000
+ */
+public class XmlDataManagerImpl implements XmlDataManager{
+    
+    private XmlTask xmlTask;
+    
+    public double getBill (Building building, int flatNumber){
+        return xmlTask.getBill(building.getStreet(), building.getNumber(), flatNumber);
+    }
+    public Flat getFlat (Building building, int flatNumber){
+        return xmlTask.getFlat(building, flatNumber);
+    }
+    public void setTariff (String tariffName, double newValue){
+        try {
+            xmlTask.setTariffs(tariffName, newValue);
+        } catch (IOException ex) {
+            Logger.getLogger(XmlDataManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void addRegistration (Building building, int flatNumber, Registration registrations) {
+        try {
+            xmlTask.addRegistration(building.getStreet(), building.getNumber(), flatNumber, registrations.getData().getYear(), registrations.getData().getMonth(), 
+                    registrations.getColdwater(), registrations.getHotwater(), registrations.getElectricity(), registrations.getGas());
+        } catch (IOException ex) {
+            Logger.getLogger(XmlDataManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
